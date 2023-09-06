86c8581280530cb2c16f2bfa07be374954b893b3
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-09-06 15:21:09 +0530

    Init commit

---
+++ game.js
@@ -0,0 +1,17 @@
+let gameStartButton = document.querySelector("button");
+let gameBoard = document.getElementById("game-board");
+let score = document.getElementById("score");
+let highestScore = document.getElementById("highest-score");
+let food = document.getElementById("food");
+let snake = document.getElementsByClassName("snake");
+
+let foodX = Math.floor(30 * Math.random()) + 1;
+console.log(foodX);
+let foodY = Math.round(30 * Math.random()) + 1;
+console.log(foodY);
+// gameStartButton.onclick = foodLocation;
+function foodLocation() {
+  score.style.backgroundColor = "lightblue";
+  //   console.log(score);
+  food.style.gridArea = foodX / foodY;
+}
