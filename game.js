969aca5a6ff46591b7d59ece5695402b79c6d724
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-09-19 19:40:52 +0530

    Updating the file

--- game.js
+++ game.js
@@ -1,15 +1,15 @@
-let gameStartButton = document.querySelector("button");
 let gameBoard = document.getElementById("game-board");
 let score = document.getElementById("score");
 let highestScore = document.getElementById("highest-score");
-let food = document.getElementById("food");
-let snake = document.getElementsByClassName("snake");
 
-let foodX = Math.floor(30 * Math.random()) + 1;
-console.log(foodX);
-let foodY = Math.round(30 * Math.random()) + 1;
-console.log(foodY);
-// gameStartButton.onclick = foodLocation;
-function foodLocation() {
-  food.style.gridArea = 16 / 6;
-}
+let foodX = 15,
+  foodY = 10;
+
+const updateFoodPosition = () => {
+  // Passing a random 1 - 30 value as food position
+  foodX = Math.floor(Math.random() * 30) + 1;
+  console.log(foodX);
+  foodY = Math.floor(Math.random() * 30) + 1;
+  console.log(foodY);
+};
+updateFoodPosition();
