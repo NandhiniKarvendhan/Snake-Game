af1d84a0165b6b24c132ad7ff939cfe8d48d48bb
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-09-30 17:55:35 +0530

    Updating the file

--- game.js
+++ game.js
@@ -48,16 +48,16 @@ const updateFoodPosition = () => {
 
 // Changing snake direction according to aroow keys
 const changeDirection = (e) => {
-  if (e.key == "ArrowLeft") {
+  if ((e.key == "ArrowLeft") & (velocityX != 1)) {
     velocityX = -1;
     velocityY = 0;
-  } else if (e.key == "ArrowRight") {
+  } else if ((e.key == "ArrowRight") & (velocityX != -1)) {
     velocityX = 1;
     velocityY = 0;
-  } else if (e.key == "ArrowUp") {
+  } else if ((e.key == "ArrowUp") & (velocityY != 1)) {
     velocityX = 0;
     velocityY = -1;
-  } else if (e.key == "ArrowDown") {
+  } else if ((e.key == "ArrowDown") & (velocityY != -1)) {
     velocityX = 0;
     velocityY = 1;
   }
